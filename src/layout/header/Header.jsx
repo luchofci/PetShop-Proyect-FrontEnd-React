ee0460154f7260d3879322a881ad81d124d362ea
luchofci <luciano.federico.c@gmail.com>
2024-02-14 19:08:32 -0300

    Header Component

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    const navigate = useNavigate()
+    const isAdmin = true;
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"))
+
+    function logout(){
+        localStorage.removeItem('currentUser');
+        localStorage.removeItem('token');
+        navigate("/")
+    }
+
+	return (
+		<header className=''>
+			{/* VER LAS CLASSNAME DE LOS NAVLINK */}
+			<NavLink to="/" className={({isActive})  => isActive ?    "nav-link active" : 'nav-link'}>Principal</NavLink>
+			<NavLink to="/contact" className="">
+				Contacto
+			</NavLink>
+			<NavLink to="/register" className="">
+				Registro
+			</NavLink>
+			<NavLink to="/about-us" className="">
+				Acerca de
+			</NavLink>
+			{currentUser ? (
+				<NavLink className="nav-link" onClick={() => logout()}>
+					Logout
+				</NavLink>
+			) : (
+				<NavLink to="/login" className="nav-link">
+					Login
+				</NavLink>
+			)}
+			{isAdmin && (
+				<>
+					<NavLink to="/admin-product" className="nav-link">
+						Admin Product
+					</NavLink>
+					<NavLink to="/admin-user" className="nav-link">
+						Admin User
+					</NavLink>
+				</>
+			)}
+		</header>
+	);
 }
